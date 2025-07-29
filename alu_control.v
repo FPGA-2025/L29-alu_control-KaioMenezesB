b8d08cd65265a7b239804a004f6b2a01f1d1d11b
KaioMenezesB <kaio_borduque@hotmail.com>
2025-07-29 14:30:14 -0300

    Atividade finalizada

--- alu_control.v
+++ alu_control.v
@@ -6,6 +6,54 @@ module ALU_Control (
     output reg [3:0] ALU_OP_o
 );
 
-// insira seu código aqui
+always @(*) begin
+    case (ALU_CO_i)
+        2'b00: 
+            ALU_OP_o = 4'b0010;
+        2'b01: 
+            case (FUNC3_i)
+                3'b000, 3'b010, 3'b011: 
+                    ALU_OP_o = 4'b1010;
+                3'b001: 
+                    ALU_OP_o = 4'b0011;
+                3'b100: 
+                    ALU_OP_o = 4'b1100;
+                3'b101: 
+                    ALU_OP_o = 4'b1110;
+                3'b110: 
+                    ALU_OP_o = 4'b1101;
+                3'b111: 
+                    ALU_OP_o = 4'b1111;
+                default: 
+                    ALU_OP_o = 4'b0000;
+            endcase
+        2'b10: 
+            case (FUNC3_i)
+                3'b000: 
+                    if (is_immediate_i || FUNC7_i == 7'b0000000)
+                        ALU_OP_o = 4'b0010;
+                    else
+                        ALU_OP_o = 4'b1010;
+                3'b111: 
+                    ALU_OP_o = 4'b0000;
+                3'b110: 
+                    ALU_OP_o = 4'b0001;
+                3'b100: 
+                    ALU_OP_o = 4'b1000;
+                3'b010: 
+                    ALU_OP_o = 4'b1110;
+                3'b011: 
+                    ALU_OP_o = 4'b1111;
+                3'b001: 
+                    ALU_OP_o = 4'b0100;
+                3'b101: 
+                    ALU_OP_o = (FUNC7_i == 7'b0100000) ? 4'b0111 : 4'b0101;
+                default: 
+                    ALU_OP_o = 4'b0000;
+            endcase
+        default: 
+            ALU_OP_o = 4'b0000;
+    endcase
+end
 
 endmodule
